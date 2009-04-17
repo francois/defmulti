d5b2b0dac61c1a5900ef092fd5c7c1ac43d1ddbf
François Beausoleil <francois@teksol.info>
2009-04-17 10:48:39 -0400

    Most of the work in passing arguments along is done

--- lib/defmulti.rb
+++ lib/defmulti.rb
@@ -11,19 +11,37 @@ module Defmulti
   def defmulti(selector, *args)
     clauses = args.pop.to_a
     clauses = clauses.sort_by {|guard, _| guard.nil? ? 1 : 0}
-    define_method(selector) do |*arguments|
-      code_to_run = clauses.detect do |guard, code|
-        if guard then
-          guard_value = instance_eval(&guard)
-          next unless guard_value
-        end
+    guards_and_clauses = clauses.inject(Hash.new) do |memo, (guard, code)|
+      suffix = [guard.inspect, code.inspect, rand.to_s.sub(".", "")].join("_")
+      guard_method_name = "defmulti_guard_#{suffix}"
+      code_method_name  = "defmulti_code_#{suffix}"
 
-        # Have to break or else #each will continue to the end
-        break code
+      if guard then
+        define_method(guard_method_name, &guard)
+      else
+        guard_method_name << "_true"
+        define_method(guard_method_name) { true }
       end
 
-      if code_to_run
-        code_to_run.respond_to?(:call) ? instance_eval(&code_to_run) : code_to_run
+      if code.respond_to?(:call) then
+        define_method(code_method_name, &code)
+      else
+        define_method(code_method_name) do
+          code
+        end
+      end
+
+      memo[guard_method_name] = code_method_name
+      memo
+    end
+
+    define_method(selector) do |*arguments|
+      code_to_run = guards_and_clauses.detect do |guard, code|
+        break code if send(guard, *arguments)
+      end
+
+      if code_to_run then
+        send(code_to_run, *arguments)
       else
         raise MissingGuardClause.new(self, selector)
       end
