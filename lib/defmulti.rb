c52332feb9d5b7ecafe135c948ead2004be0fe9e
François Beausoleil <francois@teksol.info>
2009-04-17 10:23:56 -0400

    Fixed that no guard clause match when no else fails with exception

--- lib/defmulti.rb
+++ lib/defmulti.rb
@@ -12,14 +12,20 @@ module Defmulti
     clauses = args.pop.to_a
     clauses = clauses.sort_by {|guard, _| guard.nil? ? 1 : 0}
     define_method(selector) do |*arguments|
-      clauses.each do |guard, code|
+      code_to_run = clauses.detect do |guard, code|
         if guard then
           guard_value = instance_eval(&guard)
           next unless guard_value
         end
 
         # Have to break or else #each will continue to the end
-        break code.respond_to?(:call) ? instance_eval(&code) : code
+        break code
+      end
+
+      if code_to_run
+        code_to_run.respond_to?(:call) ? instance_eval(&code_to_run) : code_to_run
+      else
+        raise MissingGuardClause.new(self, selector)
       end
     end
   end
